f464de0370fe69d3fffdc4603f71568e97d5308b
HomePC <ira.intertech@gmail.com>
2016-02-10 19:25:33 +0200

    tablet

--- dist/js/untils.js
+++ dist/js/untils.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     jQuery('.slider').owlCarousel({
         loop:true,
         nav:false,
-        autoHeight:true,
+       // autoHeight:true,
         autoplay:true,
         responsive:{
             0:{
