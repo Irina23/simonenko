97d18be9f658b8e785ea052e2379740695f67890
HomePC <ira.intertech@gmail.com>
2016-05-06 17:32:37 +0300

    home page new

--- dist/js/untils.js
+++ dist/js/untils.js
@@ -148,6 +148,21 @@ jQuery(document).ready(function() {
 
 
         });
+
+
+
+
+        var ias = jQuery.ias({
+            container: '.list_product_home',
+            item: '.product',
+            pagination: '.pagination',
+            next: '.next a'
+        });
+        ias.extension(new IASTriggerExtension({
+            text: 'Загрузить еще' // optionally
+        }));
+
+
     });
 
     jQuery(".payment").validate({
